8ab1be5cdce5e013228c1020d6e0c87d66940faf
alway1 <alway@email.wm.edu>
2019-10-07 20:52:32 -0400

    My asc script for #17

--- sandbox/way.py
+++ sandbox/way.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
-print(3)
-print(4)
+import os
+import string
+
+for fname in os.listdir('.'):
+  if fname.endswith('.txt', '.asc'):
+    data = open(fname).readlines()
+
+    listnum = []
+    listalpha = []
+    for line in data:
+      line = line.strip()
+      if str(line).startswith(('0,1,2,3,4,5,6,7,8,9')):
+        listnum.append()
+      if line[:1].isalpha():
+        listalpha.append()
+    print(len(listnum))
+
+    print(len(a) == len(b))
