6077aee5a2ed71bd8ee4bd61a648b1d6abe37fed
dt-woods <davisler@gmail.com>
2019-10-11 00:30:01 -0400

    created netcdf read/write scripts

---
+++ scripts/nc_read.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+#
+# nc_write.py
+#
+# LAST EDIT: 2019-10-10
+#
+# This script writes an NetCDF file.
+
+###############################################################################
+# IMPORT MODULES
+###############################################################################
+import os
+import os.path
+
+import numpy
+from scipy.io import netcdf
+
+###############################################################################
+# FUNCTIONS
+###############################################################################
+def print_keys(my_obj):
+    """
+    Name:     print_keys
+    Inputs:   dict, dictionary (my_obj)
+    Outputs:  None
+    Features: Prints key-value pairs for an ordered dictionary
+    """
+    try:
+        for key in my_obj.keys():
+            value = my_obj[key]
+            if isinstance(value, bytes):
+                value = value.decode("utf-8")
+
+            print(f"  {key}: {value}")
+    except AttributeError as e:
+        print("Object has no keys.", e)
+
+
+###############################################################################
+# MAIN
+###############################################################################
+try:
+    my_dir = os.environ['DS_WORKSPACE']
+except:
+    my_dir = os.path.expanduser("~")
+
+my_file = "test.nc"
+nc_path = os.path.join(my_dir, my_file)
+
+# Open a new NetCDF file
+if os.path.isfile(nc_path):
+    f = netcdf.netcdf_file(nc_path, 'r')
+
+    # Print file attributes:
+    print("File attributes for %s" % (f.filename))
+    print_keys(f._attributes)
+
+    print("File dimensions")
+    print_keys(f.dimensions)
+
+    print("File variables")
+    print_keys(f.variables)
+
+    # Print variable attributes:
+    for key in f.variables.keys():
+        print(key)
+        print_keys(f.variables[key]._attributes)
+
+    # Read data
+    if 'image' in f.variables:
+        # Copy data out of file avoids error when closing the file.
+        my_data = f.variables['image'].data.copy()
+        print("Read %d bands, %d rows, %d cols" % my_data.shape)
+
+    f.close()
---
+++ scripts/nc_write.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+#
+# nc_write.py
+#
+# LAST EDIT: 2019-10-10
+#
+# This script writes an NetCDF file.
+
+###############################################################################
+# IMPORT MODULES
+###############################################################################
+import os
+import os.path
+
+import datetime
+import numpy
+from scipy.io import netcdf
+
+
+##############################################################################
+# GLOBAL VARIABLES:
+##############################################################################
+ERROR_VAL = -9999
+
+
+###############################################################################
+# FUNCTIONS
+###############################################################################
+def nc_history():
+    """
+    Name:     nc_history
+    Input:    None.
+    Output:   string (my_str)
+    Features: Returns a string for netCDF file history field based on the file's
+              creation date
+    """
+    my_str = "created %s" % datetime.date.today()
+    return my_str
+
+
+
+###############################################################################
+# MAIN
+###############################################################################
+# Create a system varianble called "DS_WORKSPACE" and save working path to it
+try:
+    my_dir = os.environ['DS_WORKSPACE']
+except:
+    my_dir = os.path.expanduser("~")
+
+my_file = "test.nc"
+nc_path = os.path.join(my_dir, my_file)
+
+# Create a new NetCDF file
+f = netcdf.netcdf_file(nc_path, 'w')
+
+if True:
+    # Write file attributes:
+    f.history = nc_history()
+    f.contact = 'Tyler W. Davis (twdavis@wm.edu)'
+    f.title = 'Example data created for Spatial Data Discovery'
+    f.note = 'You may safely delete this file'
+    f.institution = 'William & Mary'
+
+    # Let's assume we have a random color image:
+    my_obs = numpy.random.randint(255, size=(3,1000,1000))
+
+    # Create y dimension & variable:
+    f.createDimension('y', 1000)
+    y = f.createVariable('y', 'i', ('y',) )
+    y[:] = numpy.arange(0, 1000, 1, int)
+    y.units = 'pixels'
+
+    # Create x dimension & variable:
+    f.createDimension('x', 1000)
+    x = f.createVariable('x', 'i', ('x',))
+    x[:] = numpy.arange(0, 1000, 1, int)
+    x.units = 'pixels'
+
+    # Create color dimension & variable
+    f.createDimension('color', 3)
+    color = f.createVariable('color', 'i', ('color',))
+    color.standard_name = 'RGB'
+    color.units = 'red green blue color bands'
+
+    # Create data variable:
+    img = f.createVariable('image', 'i', ('color','y','x'))
+    img._FillValue = ERROR_VAL
+    img.missing_value = ERROR_VAL
+    img.long_name = 'Random RGB color band image'
+    img.units = 'none'
+    img[0] = numpy.random.randint(255, size=(1000,1000))
+    img[1] = numpy.random.randint(255, size=(1000,1000))
+    img[2] = numpy.random.randint(255, size=(1000,1000))
+
+f.close()
