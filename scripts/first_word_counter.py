388ca3e4be104d5f236584d7e1f0c96b1492d74f
rhan9 <ryanhan9@outlook.com>
2020-02-10 23:29:34 -0500

    Beginning development on my utility script. Addresses #37

--- scripts/first_word_counter.py
+++ scripts/first_word_counter.py
@@ -1,25 +1,63 @@
+# first_word_counter.py
+
+# By: Ryan Han
+
+# VERSION 1.0
+
+# Last Edit: 2020-02-10
+
+# This script takes .txt or .md files and a word of your choice and returns
+# each sentence/sentences where the specified word is the first word. The code
+# also prints out the number of times the 'word' appears as the first word.
+
+####################
+# Required Module  #
+####################
 import argparse
 
+####################
+#    Function      #
+####################
+
 def first_word_counter():
-    file_name = input("Enter file name: ") # Asking for the file name
+    # Asking user for file name and word
+    file_name = input("Enter file name: ")
     word = input("Enter a word you would like to count in the file: ")
+
+    # Allows me to press enter to access default sample .txt file
     if len(file_name) < 1 : file_name = "mbox-short.txt"
+
+    # Opening specified file
     open_file = open(file_name) # Opening said file name
-    count = 0 # Establishing "count" at 0
-    for line in open_file: # starting a "for" loop that will examine every line in the file
-        line = line.rstrip() # striping the lines of white space
-        #if not line.startswith("From:"): continue   #if the lines start with "From:" then you can skip them and go back to top of the loop
-        if line.startswith(word): # if the line starts with "From" then you can move on to the rest of the program.
-            words = line.split() # each line is split into each character, then those characters make up the variable words.
-            print(line) # all of the lines that start with From will have the character in subsection 1 printed.
-            count = count + 1 # each time the loop hits here, a 1 is added to the total count
+
+    # Establishing "count" at 0
+    count = 0
+    # Starting loop to examine every line in file
+    for line in open_file:
+        # striping the lines of white space
+        line = line.rstrip()
+        # Once line with word is found
+        if line.startswith(word):
+            # each line split into each characters, the characters make up the variable words.
+            words = line.split()
+            # print whole sentence
+            print(line)
+            # at the end of loop add 1 to count
+            count = count + 1
+
+    # Proper grammar
     if count == 1:
         print("There is " + str(count) + " line in the file with " + word + " as the first word.")
     else:
         print("There are " + str(count) + " lines in the file with " + "'" + word + "'" + " as the first word.")
 
+###################
+#      Main       #
+###################
 if __name__ == '__main__':
+    # --help command line description
     parser = argparse.ArgumentParser(
-    description = "Input a .txt file and word of your choice. This script will figure out how many times your word starts a sentence in the file.")
+    description = "Input a .txt/.md file and word of your choice. This script will figure out how many times your word starts a sentence in the file.")
     args = parser.parse_args()
+
     first_word_counter()
