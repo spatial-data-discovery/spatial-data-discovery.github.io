78009b5ac408cc404031b96c1c341126e6491cbc
Hannah Slevin <54992420+hannahslevin@users.noreply.github.com>
2020-09-03 18:36:34 -0400

    Update grade_calculator.py

--- scripts/grade_calculator.py
+++ scripts/grade_calculator.py
@@ -15,150 +15,118 @@
 #######################
 import argparse
 
-################################
-# Calculating Percentage Grade #
-################################
-
-#This program will calculate your Semester grade based on entering percentages from each assingment
-#
-#Create a list to store each grade
-grades = []
-#
-#Before each section a line will print distinguishing which section of the grade is being taken as an input
-#
-#Print section one
-print('Section 1: Discussion Grade')
-#
-#The user is prompted to enter their discussion grade
-#The script casts it as an int and assigns the integer to variable "discuss"
-discuss = int(input("Enter percentage grade for your Discussion Meetings: "))
-#discussion grade percentage is changed to point value
-discuss = (discuss/100)*24
-#discussion grade is appended to the grade list
-grades.append(discuss)
-#
-#
-#Print Section two
-print('Section 2: Assignments')
-#
-#The user is prompted to enter their About the coder assignment grade
-#The script casts it as an int and assigns the integer to variable "atc"
-atc = int(input("Enter percentage grade for the About the Coder assingment: "))
-#The about the coder grade percentage is changed to point value
-atc = (atc/100)*5
-#The about the coder grade is appended to the grade list
-grades.append(atc)
-#
-#The user is prompted to enter their Utility Script assignment assignment grade
-#The script casts it as an int and assigns the integer to variable "us"
-us = int(input("Enter percentage grade for the Utility Script assingment: "))
-#The utlity script grade percentage is changed to point value
-us = (us/100)*5
-#The utility script grade is appended to the grade list
-grades.append(us)
-#
-#The user is prompted to enter their Sparse Data Challenge Assignment grade
-#The script casts it as an int and assigns the integer to variable "sdc"
-sdc = int(input("Enter percentage grade for the Sparse Data Challenge assingment: "))
-#The sparse data challenge grade percentage is changed to point value
-sdc = (sdc/100)*5
-#The sparse data challenge grade is appended to the grade list
-grades.append(sdc)
-#
-#The user is prompted to enter their Conversion Scripts Assignment grade
-#The script casts it as an int and assigns the integer to variable "cs"
-cs = int(input("Enter percentage grade for The Conversion Scripts assingment: "))
-#The conversion scripts grade percentage is changed to point value
-cs = (cs/100)*10
-#The conversion scripts grade is appended to the grade list
-grades.append(cs)
-#
-
-#The user is prompted to enter their The PEP8 Assignment grade
-#The script casts it as an int and assigns the integer to variable "pep8"
-pep8 = int(input("Enter percentage grade for The PEP8 assingment: "))
-#The PEP8 grade percentage is changed to point value
-pep8 = (pep8/100)*5
-#The PEP8 grade is appended to the grade list
-grades.append(pep8)
-#
-#
-#Print Section three
-#The user is prompted to enter their reports grade
-#The script casts it as an int and assigns the integer to variable "reports"
-print('Section 3: Reports')
-reports = int(input("Enter percentage grade for your reports: "))
-#The reports grade percentage is changed to point value
-reports = (reports/100)*24
-#The reports grade is appended to the grade list
-grades.append(reports)
-#
-#
-#Print Section four
-print('Section 4: Project')
-#The user is prompted to enter their project grade
-#The script casts it as an int and assigns the integer to variable "proj"
-proj = int(input("Enter percentage grade for your project: "))
-#The project grade percentage is changed to point value
-proj = (proj/100)*22
-#The project grade is appended to the grade list
-grades.append(proj)
-#print(grades) #uncomment to check that program correctly appended grades to grade list
-#
-#Calculate the sum of points for each assignment, since the point totals add up to 100, there is no need to divide by a total
-total_pcnt = sum(grades)
-#Print final perentage grade
-print("Final Grade (in percentage points):",total_pcnt,"%")
-#
-#
-############################
-# Calculating Letter Grade #
-############################
-#Calculate letter grade based on grade distribution found in syllabus using a series of if statements
-#If the user scores over 93%, print A & a message
-if total_pcnt > 93.0:
-    print("Final Letter Grade: A")
-    print("Congratulations on Superior Mastery!")
-#If the user scores between 90% and 92.99%, print A-
-elif (total_pcnt >= 90) & (total_pcnt <= 92.99):
-    print("Final Letter Grade: A-")
-#If the user scores between 87% and 89.99%, print B+
-elif (total_pcnt >= 87) & (total_pcnt <= 89.99):
-    print("Final Letter Grade: B+")
-#If the user scores between 83% and 86.99%, print B & a message
-elif (total_pcnt >= 83) & (total_pcnt <= 86.99):
-    print("Final Letter Grade: B")
-    print("Congratulations on Good Mastery!")
-#If the user scores between 80% and 82.99%, print B-
-elif (total_pcnt >= 80) & (total_pcnt <= 82.99):
-    print("Final Letter Grade: B-")
-#If the user scores between 77% and 79.99%, print C+
-elif (total_pcnt >= 77) & (total_pcnt <= 79.99):
-    print("Final Letter Grade: C+")
-#If the user scores between 73% and 77.99%, print C & a message
-elif (total_pcnt >= 73) & (total_pcnt <= 77.99):
-    print("Final Letter Grade: C")
-    print("Your performance is satisfactory")
-#If the user scores between 70% and 72.99%, print C-
-elif (total_pcnt >= 70) & (total_pcnt <= 72.99):
-    print("Final Letter Grade: C-")
-#If the user scores between 67% and 69.99%, print D+
-elif (total_pcnt >= 67) & (total_pcnt <= 69.99):
-    print("Final Letter Grade: D+")
-#If the user scores between 60% and 66.99%, print D & a message
-elif (total_pcnt >= 60) & (total_pcnt <= 66.99):
-    print("Final Letter Grade: D")
-    print("Unfortunately, your performance is less-than-satisfactory")
-#If the user scores under 60%, print F & a message
-elif total_pcnt <60:
-    print("Final Letter Grade: F")
-    print("Unfortunately, your performance is not satisfactory")
-    print("You will need to retake this course")
 
 ###################
 #      Main       #
 ###################
 if __name__ == '__main__':
+    
+    ################################
+    # Calculating Percentage Grade #
+    ################################
+
+    #This program will calculate your Semester grade based on entering percentages from each assingment
+    #
+    #Create a list to store each grade
+    grades = []
+    #
+    #Before each section a line will print distinguishing which section of the grade is being taken as an input
+    #
+    print('Section 1: Discussion Grade')
+    #
+    #The user is prompted to enter a grade for each assingment
+    #The percentage grade will be cast as an integer an assinged to a variable 
+    #Each percentage will be recalculated from percent to point, then appended to the grades list
+    #
+    #Discussion Grade
+    discuss = int(input("Enter percentage grade for your Discussion Meetings: "))
+    discuss = (discuss/100)*24
+    grades.append(discuss)
+    #
+    #
+    print('Section 2: Assignments')
+    #
+    #About the Coder Grade
+    atc = int(input("Enter percentage grade for the About the Coder assingment: "))
+    atc = (atc/100)*5
+    grades.append(atc)
+    #
+    #Utility Script Grade
+    us = int(input("Enter percentage grade for the Utility Script assingment: "))
+    us = (us/100)*5
+    grades.append(us)
+    #
+    #Sparse Data Challenge Grade
+    sdc = int(input("Enter percentage grade for the Sparse Data Challenge assingment: "))
+    sdc = (sdc/100)*5
+    grades.append(sdc)
+    #
+    #Conversion Script Grade
+    cs = int(input("Enter percentage grade for The Conversion Scripts assingment: "))
+    cs = (cs/100)*10
+    grades.append(cs)
+    #
+    #PEP8 Assignment grade
+    pep8 = int(input("Enter percentage grade for The PEP8 assingment: "))
+    pep8 = (pep8/100)*5
+    grades.append(pep8)
+    #
+    #
+    print('Section 3: Reports')
+    #Reports grade    
+    reports = int(input("Enter percentage grade for your reports: "))
+    reports = (reports/100)*24
+    grades.append(reports)
+    #
+    #Print Section four
+    print('Section 4: Project')
+    #
+    #Project Grade
+    proj = int(input("Enter percentage grade for your project: "))
+    proj = (proj/100)*22
+    grades.append(proj)
+    #
+    #print(grades) #uncomment to check that program correctly appended grades to grade list
+    #
+    #Calculate the sum of points for each assignment
+    total_pcnt = sum(grades)
+    #Print final perentage grade
+    print("Final Grade (in percentage points):",total_pcnt,"%")
+    #
+    #
+    ############################
+    # Calculating Letter Grade #
+    ############################
+    #Calculate letter grade based on grade distribution found in syllabus using a series of if statements
+    if total_pcnt > 93.0:
+        print("Final Letter Grade: A")
+        print("Congratulations on Superior Mastery!")
+    elif (total_pcnt >= 90) & (total_pcnt <= 92.99):
+        print("Final Letter Grade: A-")
+    elif (total_pcnt >= 87) & (total_pcnt <= 89.99):
+        print("Final Letter Grade: B+")
+    elif (total_pcnt >= 83) & (total_pcnt <= 86.99):
+        print("Final Letter Grade: B")
+        print("Congratulations on Good Mastery!")
+    elif (total_pcnt >= 80) & (total_pcnt <= 82.99):
+        print("Final Letter Grade: B-")
+    elif (total_pcnt >= 77) & (total_pcnt <= 79.99):
+        print("Final Letter Grade: C+")
+    elif (total_pcnt >= 73) & (total_pcnt <= 77.99):
+        print("Final Letter Grade: C")
+        print("Your performance is satisfactory")
+    elif (total_pcnt >= 70) & (total_pcnt <= 72.99):
+        print("Final Letter Grade: C-")
+    elif (total_pcnt >= 67) & (total_pcnt <= 69.99):
+        print("Final Letter Grade: D+")
+    elif (total_pcnt >= 60) & (total_pcnt <= 66.99):
+        print("Final Letter Grade: D")
+        print("Unfortunately, your performance is less-than-satisfactory")
+    elif total_pcnt <60:
+        print("Final Letter Grade: F")
+        print("Unfortunately, your performance is not satisfactory")
+        print("You will need to retake this course")
+
     # --help command line description
     parser = argparse.ArgumentParser(
     description = "This script will calculate your final grade based on the percentage grades that you input for each assignment.")
