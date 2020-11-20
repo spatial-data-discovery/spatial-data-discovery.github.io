b7e81c1f828fabdd05540db2ec67b009af09ad27
natallzl <nalarsen@email.wm.edu>
2020-11-20 12:47:45 -0500

    Updated to reflect PEP8 assignment

--- scripts/what_to_watch.py
+++ scripts/what_to_watch.py
@@ -1,61 +1,68 @@
-﻿#what_to_watch.py
+#!/usr/bin/env python3
 #
-# Author: Natalie Larsen
+# what_to_watch.py
 #
-# Last Edit: 2020-09-04
+# AUTHOR: Natalie Larsen
 #
-#This script takes movie or show titles input by the user
-#and outputs one for the user to watch next.
-#Example user input:
-#The Princess Bride,,1917,,Monsters, Inc.,,Once Upon a Time... In Hollywood
+# LAST EDIT: 2020-11-20
+#
+# Purpose:
+# This script takes movie or show titles input by the user
+# and output one for the user to watch next.
 # If the result isn’t satisfying,
-#the user has the option to re-pick.
+# the user has the option to re-pick.
 #
-#Required Modules
-import random
+##############################################################################
+# REQUIRED MODULES
+##############################################################################
 import argparse
 import sys
+import random
 
-#Randomly pick a title from a list, and return choice
+
+##############################################################################
+# FUNCTIONS
+##############################################################################
 def pick_title():
-    to_watch = random.choice(user_list)
+    """Randomly choose title from user input,
+    and return chosen title to user.
+    """
+    to_watch = random.choice(user_list) #Choose title from user list
     print("You should watch:", to_watch)
 
-#Give user the option to re-pick
 def new_title():
+    """Give the user the option to re-pick."""
     pick_again = input("Do you want us to pick again? (y/n): ")
-    #If user wants to re-pick, run pick_title function
     if pick_again == "y":
         pick_title()
         new_title()
     elif pick_again == "n":
         print("Happy viewing!")
-    #Handle user inputs other than y or n
+        sys.exit()
     else:
         print("Sorry we didn't get that, please answer y or n.")
         new_title()
 
 
+##############################################################################
+# MAIN
+##############################################################################
 if __name__ == "__main__":
-    #Support for --help or -h command-line arguments
-    p = argparse.ArgumentParser(
-    description = "This script takes a list of movies/shows input by the user \
-    (ex. movie1,,movie2,,movie3,,etc.) and returns one to watch.")
+    script_desc = "This script takes a list of movies/shows input by the user \
+                  (ex. movie1,,movie2,,movie3,,etc.) and returns one to watch."
+    p = argparse.ArgumentParser(description = script_desc)
     args = p.parse_args()
 
-    #Get user input,
-    #taking into account punctuation in titles
-    print("Can't decide what to watch? Let us help!")
-    user_input = input("Enter a list of movies/shows, separating titles with two commas(A,,B):\n")
-
-    #Turn input into list
+    print("\nCan't decide what to watch? Let us help!\n")
+    print("Enter a list of movies/shows!")
+    user_input = input("Separate titles with two commas(A,,B,,etc.):\n")
     if ',,' in user_input:
-        user_list = user_input.split(',,')
+        user_list = user_input.split(',,') #Turn input into list
         print("You input the following choices:", user_list)
-    #Handle unacceptable user inputs
     else:
         print("Please enter more than one title, separated by two commas(,,).")
         sys.exit()
 
     pick_title()
+
     new_title()
