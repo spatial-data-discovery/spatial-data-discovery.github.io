6f6f7e77a3652690f5f69bf1f28b980352b5969c
Alaa Mokbel <87028759+amokbel@users.noreply.github.com>
2021-09-15 00:12:35 -0400

    added run command

--- scripts/word_bank.py
+++ scripts/word_bank.py
@@ -2,11 +2,18 @@ def make_word_bank(source, sep = ' '):
     word_list = []
     cur_word = ''
     for letter in source:
-        if (letter != sep and letter not in '?!.,:;[]()"' and letter != '\n'):
-            cur_word+= letter
+        if cur_word in word_list:
+            cur_word = ''
+            continue
+        elif (letter != sep and letter not in '?!.,:;[]()"' and letter != '\n'):
+            cur_word += letter
         elif ((letter == sep or letter =='\n') and cur_word !=''):
             word_list+= [cur_word]
             cur_word = ''
-    if cur_word !='':
-        word_list+=[cur_word]
+        elif cur_word !='':
+            word_list+=[cur_word]
+            cur_word = ''
     return word_list
+
+source = input('what sentence do you want a word bank for?')
+make_word_bank(source)
