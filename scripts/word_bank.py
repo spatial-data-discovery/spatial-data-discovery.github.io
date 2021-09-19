a79f8488d67227202f74aa820209320d13d500f5
Alaa Mokbel <87028759+amokbel@users.noreply.github.com>
2021-09-18 23:58:43 -0400

    added command line argument (-h / --help) #75 #71
    I added the command line argument as well as fixed a few lines (18-19) that ensured the last word doesn't get cut off (i didn't notice before, but it used to not add the last word to the bank)

--- scripts/word_bank.py
+++ scripts/word_bank.py
@@ -1,19 +1,30 @@
+import argparse
+
 def make_word_bank(source, sep = ' '):
     word_list = []
     cur_word = ''
     for letter in source:
-        if cur_word in word_list:
+        if cur_word in word_list: #if word is already in word bank, skip it
             cur_word = ''
             continue
-        elif (letter != sep and letter not in '?!.,:;[]()"' and letter != '\n'):
+        elif (letter != sep and letter not in '?!.,:;[]()"' and letter != '\n'): #if the string pulled is not the seperator, punctuations, or a new line, add it
             cur_word += letter
-        elif ((letter == sep or letter =='\n') and cur_word !=''):
+        elif ((letter == sep or letter =='\n') and cur_word !=''): #this line ensures that cur_word is empty when new string is added to it
             word_list+= [cur_word]
             cur_word = ''
-        elif cur_word !='':
+        elif cur_word !='':  #this adds the word being built into the word bank
             word_list+=[cur_word]
             cur_word = ''
+    if cur_word not in word_list: #this line deals with the very last set of letters the script pulls; if it's not in word bank already, it adds it
+        word_list+=[cur_word]
     return word_list
 
-source = input('what sentence do you want a word bank for?')
-make_word_bank(source)
+if __name__ == "__main__":
+    # Create an ArgumentParser class object for dealing with commandline args
+    p = argparse.ArgumentParser(
+    description="Creates a word bank of all unique words in inserted sentence.")
+
+    args = p.parse_args()
+
+source = input('what sentence do you want a word bank for? ')
+print (make_word_bank(source))
